1efb459be539bebc72d96aa8946a98759fd02e3c
Michal Dobrzycki <dobrzycki@gmail.com>
2022-02-16 21:25:52 +0100

    Removed Selenium WebDriver from main branch

---
+++ SeleniumWebdriver/PageObjectPattern/pages/LoginPage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumWebdriver.PageObjectPattern.pages
+{
+    public class LoginPage
+    {
+        private IWebDriver driver;
+    }
+}
