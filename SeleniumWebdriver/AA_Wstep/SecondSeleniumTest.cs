ed6875a88c0b571bd80218d6b55abc0272acb049
tsmierzchala <tomasz.smierzchala@forcom.com.pl>
2022-02-17 15:35:16 +0100

    selects with sorted

--- SeleniumWebdriver/AA_Wstep/SecondSeleniumTest.cs
+++ SeleniumWebdriver/AA_Wstep/SecondSeleniumTest.cs
@@ -25,10 +25,20 @@ namespace SeleniumWebdriver.AA_Wstep
             driver.FindElement(By.Id("password")).SendKeys("secret_sauce");
             driver.FindElement(By.Id("login-button")).Click();
 
+            List<double> price = new List<double>();
+
             IList<IWebElement> cards = driver.FindElements(By.ClassName("inventory_item"));
             foreach (IWebElement card in cards) {
                 IWebElement inventory_item_name = card.FindElement(By.ClassName("inventory_item_name"));
                 Console.WriteLine(inventory_item_name.Text);
+
+                IWebElement inventory_item_price = card.FindElement(By.ClassName("inventory_item_price"));
+                var new_price = inventory_item_price.Text[1..];
+                price.Add(double.Parse(new_price.Replace('.', ',')));
+            }
+            price.Sort();
+            foreach(double inventory_item in price) {
+                Console.WriteLine(inventory_item);
             }
         }
 
