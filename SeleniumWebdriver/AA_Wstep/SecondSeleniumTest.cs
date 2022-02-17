a2299dd11d691fa25bce79a7dcc17b0e9cedb8d6
Tomasz Śmierzchała <tsmierzchala@gmail.com>
2022-02-17 21:07:09 +0100

    New result

--- SeleniumWebdriver/AA_Wstep/SecondSeleniumTest.cs
+++ SeleniumWebdriver/AA_Wstep/SecondSeleniumTest.cs
@@ -3,6 +3,7 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace SeleniumWebdriver.AA_Wstep
 {
@@ -25,26 +26,38 @@ namespace SeleniumWebdriver.AA_Wstep
             driver.FindElement(By.Id("password")).SendKeys("secret_sauce");
             driver.FindElement(By.Id("login-button")).Click();
 
-            List<double> price = new List<double>();
+            //List<double> prices = new List<double>();
+            Dictionary<string, double> results = new Dictionary<string, double>();
 
             IList<IWebElement> cards = driver.FindElements(By.ClassName("inventory_item"));
             foreach (IWebElement card in cards) {
                 IWebElement inventory_item_name = card.FindElement(By.ClassName("inventory_item_name"));
-                Console.WriteLine(inventory_item_name.Text);
+                string title = inventory_item_name.Text;
+                //Console.WriteLine(title);
 
                 IWebElement inventory_item_price = card.FindElement(By.ClassName("inventory_item_price"));
-                var new_price = inventory_item_price.Text[1..];
-                price.Add(double.Parse(new_price.Replace('.', ',')));
+                string new_price = inventory_item_price.Text[1..].Replace('.', ',');
+                double price = double.Parse(new_price);
+                //prices.Add(price);
+
+                results.Add(title, price);
             }
-            price.Sort();
-            foreach(double inventory_item in price) {
-                Console.WriteLine(inventory_item);
+            //prices.Sort();
+            //foreach(double price in prices) {
+            //    Console.WriteLine(price);
+            //}
+
+            var ordered = results.OrderBy(value => value.Value);
+
+            foreach (KeyValuePair<string, double> entry in ordered) { 
+                Console.WriteLine($"{entry.Key}: {entry.Value}");
             }
         }
 
         [TearDown]
         public void TearDown()
         {
+            driver.Quit();
         }
     }
 
