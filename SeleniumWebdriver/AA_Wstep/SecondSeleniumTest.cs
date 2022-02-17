cfdf7ad9c59fc7fbe16308cafc1bae7205f5052f
tsmierzchala <tomasz.smierzchala@forcom.com.pl>
2022-02-17 14:52:41 +0100

    Find inventory_item_name

--- SeleniumWebdriver/AA_Wstep/SecondSeleniumTest.cs
+++ SeleniumWebdriver/AA_Wstep/SecondSeleniumTest.cs
@@ -1,5 +1,8 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
 
 namespace SeleniumWebdriver.AA_Wstep
 {
@@ -11,11 +14,22 @@ namespace SeleniumWebdriver.AA_Wstep
         [SetUp]
         public void Setup()
         {
+            driver = new ChromeDriver();
+            driver.Url = "https://www.saucedemo.com/";
         }
 
         [Test]
         public void SearchForMyCompanyShouldReturnSomeResults()
         {
+            driver.FindElement(By.Id("user-name")).SendKeys("standard_user");
+            driver.FindElement(By.Id("password")).SendKeys("secret_sauce");
+            driver.FindElement(By.Id("login-button")).Click();
+
+            IList<IWebElement> cards = driver.FindElements(By.ClassName("inventory_item"));
+            foreach (IWebElement card in cards) {
+                IWebElement inventory_item_name = card.FindElement(By.ClassName("inventory_item_name"));
+                Console.WriteLine(inventory_item_name.Text);
+            }
         }
 
         [TearDown]
